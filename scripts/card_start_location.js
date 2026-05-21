02a691a7c30cf82318c59182d580ec7681c6c0d2
Inoyu <67546535+Inoyuuuuu@users.noreply.github.com>
2026-05-21 20:23:57 +0200

    separated movement and start position script

---
+++ scripts/card_start_location.js
@@ -0,0 +1,35 @@
+const movableCardContainer = document.getElementById('movable-card-container');
+const movableCards_loc = document.querySelectorAll('.movable');
+
+let amountCards = movableCards_loc.length;
+let angleSize = 360 / amountCards;
+let radius = 300; //in px
+
+let angleOffset = Math.random() * 360;
+
+for (let i = 0; i < amountCards; i++) {
+    const card = movableCards_loc[i];
+
+    let angle = i * angleSize + angleOffset;
+
+    //x0, y0 start pos of cards (cards center point)
+    const cardX = movableCardContainer.getBoundingClientRect().left - (card.offsetWidth / 2);
+    const cardY = movableCardContainer.getBoundingClientRect().top - (card.offsetHeight / 2);
+
+    //x50, y50 of movable card container thing
+    const midX = movableCardContainer.getBoundingClientRect().width / 2;
+    const midY = movableCardContainer.getBoundingClientRect().height / 2;
+
+    function toRadians(degrees) {
+        return degrees * (Math.PI / 180);
+    }
+
+    let circleX = radius * Math.cos(toRadians(angle));
+    let circleY = radius * Math.sin(toRadians(angle));
+
+    circleX = Math.round(circleX);
+    circleY = Math.round(circleY);
+
+    card.style.left = (midX + circleX + cardX) + 'px';
+    card.style.top = (midY + circleY + cardY) + 'px';
+}
--- scripts/movable_cards.js
+++ scripts/movable_cards.js
@@ -1,41 +1,7 @@
-const movableCardContainer = document.getElementById('movable-card-container');
-const movableCards = document.querySelectorAll('.movable');
-
-
-
-//random location
-
-let amountCards = movableCards.length;
-let angleSize = 360 / amountCards;
-let radius = 300; //in px
-
-for (let i = 0; i < amountCards; i++) {
-
-    const card = movableCards[i];
-    console.log(i + " movable card: " + card.id + " angle: " + i*angleSize);
-
-    //x0, y0 start pos von cards (cards-mittelpunkt)
-    const cardX = movableCardContainer.getBoundingClientRect().left - (card.offsetWidth / 2);
-    const cardY = movableCardContainer.getBoundingClientRect().top - (card.offsetHeight / 2);
-
-    //x50, y50 von movable card dings
-    const midX = movableCardContainer.getBoundingClientRect().width / 2;
-    const midY = movableCardContainer.getBoundingClientRect().height / 2;
-
-    let circleX = radius * Math.cos((i * angleSize) * (Math.PI / 180));
-    let circleY = radius * Math.sin((i * angleSize) * (Math.PI / 180));
-
-    circleX = Math.round(circleX);
-    circleY = Math.round(circleY);
-
-    console.log("circleX " + circleX + " circleY " + circleY);
-
-    card.style.left = (midX + circleX + cardX) + 'px';
-    card.style.top = (midY + circleY + cardY) + 'px';
-}
+const movableCards_mv = document.querySelectorAll('.movable');
 
 //movement even listeners
-for (const card of movableCards) {
+for (const card of movableCards_mv) {
     let isDragging = false;
     let startX, startY;
 
